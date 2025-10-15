0f4ea809d486c8d48c2eba73a6460e1a40fece25
AreebAhmd14 <areebfarooq549@gmail.com>
2025-10-16 01:08:10 +0500

    final

---
+++ app/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://leather-trend.vercel.app/',
+  generateRobotsTxt: true,
+}
+
+export default config
